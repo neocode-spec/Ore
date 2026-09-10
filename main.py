773bac0e992b7457310925d6c97da4ea5f07577c
neocode-spec <mbmurphy.tech@gmail.com>
2026-09-10 16:24:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,12 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://ore-liart.vercel.app",
+        "http://localhost:3000",
+        "*"
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
